00848fc46579f9e89f0312eb31b2bebf219a3311
white-fortune <whitefortune271828@gmail.com>
2025-10-19 08:27:47 +0600

    tailwindcss setup

--- src/features/feed/components/index.tsx
+++ src/features/feed/components/index.tsx
@@ -3,7 +3,7 @@ import "../assets/css/index.css";
 export default function Feed() {
     return (
         <>
-            <div className="feed">
+            <div className="feed text-2xl">
                 <h1>This is the feed section!</h1>
             </div>
         </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./app/app.css"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/app'
